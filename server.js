7c2f300dafc7265d9fe34794214f6271202bd3e4
Suziane Cundiff <suzianecundiff@gmail.com>
2020-08-04 19:19:05 -0400

    Update09.5
    Heroku :(

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
 const jsonServer = require('json-server');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
