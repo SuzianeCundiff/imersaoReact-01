810641d9a40f20dc3595c4934aec0b98ac57e63d
Suziane Cundiff <suzianecundiff@gmail.com>
2021-02-12 20:19:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,5 +11,6 @@ const port = process.env.PORT || 8080;
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`JSON Server is running in ${port}`);
 });
